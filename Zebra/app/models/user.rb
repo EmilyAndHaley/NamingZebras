abe355c686af7692e9682b7b749a35f3aee3e31d
NightTrain <prkmms@gmail.com>
2013-10-24 21:22:25 -0400

    Edited alerts for registration

--- Zebra/app/models/user.rb
+++ Zebra/app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates_presence_of :firstName, :lastName
 end
