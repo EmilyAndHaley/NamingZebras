568585ad8d8f574fe5e77581737fdcd7f8dab149
NightTrain <prkmms@gmail.com>
2013-11-16 15:06:43 -0500

    Fixed symptoms model

--- Zebra/db/seeds.rb
+++ Zebra/db/seeds.rb
@@ -3,11 +3,10 @@ require 'nokogiri'
 sympFile = File.open("Dataset.xml")
 @doc = Nokogiri::XML(sympFile)
 sympFile.close()
-puts 'hello'
 symptomsList = []
 
 @doc.xpath("//DisorderSign").each do |x|
 	signId = x.at('ClinicalSign').attribute('id').text()
 	name = x.at('ClinicalSign').element_children().text()
-	Symptom.where(:name => name, :signid => Integer(signId)).first_or_create
+	Symptom.where(:name => name, :signId => Integer(signId)).first_or_create
 end
